cc56aaed7a680f242f151c3c3215500fcc17164b
Sujit Kumar <sujitkumar20501@gmail.com>
2024-05-02 01:34:06 -0600

    add movement to different phases of tapping

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var maxTime = 10;
+var maxTime = 6;
 var secondsLeft = maxTime;
 var touchStarted = 0;
 var touchCount = 0;
@@ -9,8 +9,15 @@ var maxDistance = Math.sqrt(window.innerWidth * window.innerWidth + window.inner
 var maxAccuracy = 0;
 var minAccuracy = 100;
 var totalAccuracy = 0;
-var maxPhases = 4;
+var maxPhases = 15;
 var numberOfPhases = maxPhases;
+const results = [];
+var centerTouchX = 0.5 * window.innerWidth;
+var centerTouchY = 0.5 * window.innerHeight;
+var resultText = "";
+var currentPhase = ["Full Screen 1: ", "Full Screen 2: ", "Full Screen 3: ", "Big Dot 1: ", "Big Dot 2: ", "Big Dot 3: ",
+    "Big Dot 4: ", "Small Dot 1: ", "Small Dot 2: ", "Small Dot 3: ", "Small Dot 4: ", "Tiny Dot 1: ",
+    "Tiny Dot 2: ", "Tiny Dot 3: ", "Tiny Dot 4: "];
 
 // Function to calculate distance between two points
 function calculateDistance(x1, y1, x2, y2) {
@@ -50,6 +57,15 @@ function defineElements() {
     };
 }
 
+function storeResults() {
+    var averageAccuracy = totalAccuracy / touchCount;
+    if (minAccuracy > maxAccuracy) {
+        minAccuracy = 0;
+    }
+    results[maxPhases - numberOfPhases - 1] = [touchCount, Math.round(maxAccuracy * 10000) / 10000, Math.round(minAccuracy * 10000) / 10000, Math.round(averageAccuracy * 10000) / 10000,]
+    resultText = resultText + "<br />" + currentPhase[maxPhases - numberOfPhases - 1] + touchCount.toString();
+}
+
 function resartPhase() {
     secondsLeft = maxTime;
     touchStarted = 0;
@@ -69,11 +85,9 @@ function displayResults() {
     document.getElementById("touchSpace").onmousedown = null;
     document.getElementById("touchSpace").onmouseup = null;
     document.getElementById("resultPanel").style.visibility = "visible";
-    var averageAccuracy = totalAccuracy / touchCount;
-    if (minAccuracy > maxAccuracy) {
-        minAccuracy = 0;
-    }
-    document.getElementById("resultsText").innerHTML = "<br />Total number of taps: " + touchCount + "<br />Highest tap accuracy: " + Math.round(maxAccuracy * 10000) / 10000 + "<br />Lowest tap accuracy: " + Math.round(minAccuracy * 10000) / 10000 + "<br />Average tap accuracy: " + Math.round(averageAccuracy * 10000) / 10000;
+    //document.getElementById("resultsText").innerHTML = "<br />Total number of taps: " + touchCount + "<br />Highest tap accuracy: " + Math.round(maxAccuracy * 10000) / 10000 + "<br />Lowest tap accuracy: " + Math.round(minAccuracy * 10000) / 10000 + "<br />Average tap accuracy: " + Math.round(averageAccuracy * 10000) / 10000;
+    //document.getElementById("resultsText").innerHTML = "<br />T1.1: " + results[0][0] + "<br />T1.2: " + results[1][0] + "<br />T1.3: " + results[2][0] + "<br />T2: " + results[3][0] + "<br />T3: " + results[4][0] + "<br />T4: " + results[5][0];
+    document.getElementById("resultsText").innerHTML = resultText;
     document.getElementById("restart").onclick = function (e) {
         numberOfPhases = maxPhases;
         document.getElementById("touchSpace").style.width = "100%";
@@ -81,6 +95,22 @@ function displayResults() {
         document.getElementById("touchSpace").style.borderRadius = "0%";
         resartPhase();
     }
+}
+
+function changePosition() {
+    maxTime = 2;
+    newWidth = Math.random() * 85;
+    newHeight = Math.random() * 85;
+    centerTouchX = newWidth * window.innerWidth;
+    centerTouchY = newHeight * window.innerHeight;
+    document.getElementById("touchSpace").style.top = newHeight.toString() + "%";
+    document.getElementById("touchSpace").style.left = newWidth.toString() + "%";
+}
+
+function resetPosition() {
+    maxTime = 5;
+    document.getElementById("touchSpace").style.top = "50%";
+    document.getElementById("touchSpace").style.left = "50%";
 }
 
 function timerFunction() {
@@ -91,28 +121,51 @@ function timerFunction() {
         document.getElementById("timer").innerHTML = secondsLeft + "s";
         clearInterval(timer);
         numberOfPhases = numberOfPhases - 1;
+        storeResults();
+        resetPosition();
         if (numberOfPhases === 0) {
             displayResults();
         }
         // big circle - multi finger
-        if (numberOfPhases === maxPhases - 1) {
+        if (numberOfPhases === maxPhases - 3) {
             document.getElementById("touchSpace").style.width = "1.25in";
             document.getElementById("touchSpace").style.height = "1.25in";
             document.getElementById("touchSpace").style.borderRadius = "50%";
             resartPhase();
         }
+        // big moving circle - multi finger
+        if (numberOfPhases <= maxPhases - 4 && numberOfPhases >= maxPhases - 6) {
+            changePosition();
+            resartPhase();
+        }
+
         // small circle - finger size
-        if (numberOfPhases === maxPhases - 2) {
+        if (numberOfPhases === maxPhases - 7) {
             document.getElementById("touchSpace").style.width = "0.5in";
             document.getElementById("touchSpace").style.height = "0.5in";
             document.getElementById("touchSpace").style.borderRadius = "50%";
             resartPhase();
         }
+        // small moving circle - multi finger
+        if (numberOfPhases <= maxPhases - 8 && numberOfPhases >= maxPhases - 10) {
+            changePosition();
+            resartPhase();
+        }
+
         // tiny circle - half finger size
-        if (numberOfPhases === maxPhases - 3) {
+        if (numberOfPhases === maxPhases - 11) {
             document.getElementById("touchSpace").style.width = "0.25in";
             document.getElementById("touchSpace").style.height = "0.25in";
             document.getElementById("touchSpace").style.borderRadius = "50%";
+            resartPhase();
+        }
+        // tiny moving circle - multi finger
+        if (numberOfPhases <= maxPhases - 12 && numberOfPhases >= maxPhases - 14) {
+            changePosition();
+            resartPhase();
+        }
+
+        if (numberOfPhases >= maxPhases - 2) {
             resartPhase();
         }
 
