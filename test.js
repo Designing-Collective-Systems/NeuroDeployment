dc802554f39e1591d77c41c2142b1bea98639e0a
Sujit Kumar <sujitkumar20501@gmail.com>
2024-05-08 02:09:41 -0600

    added instructions before each block, audio cue, results for each section

--- test.js
+++ test.js
@@ -9,18 +9,16 @@ var maxDistance = Math.sqrt(window.innerWidth * window.innerWidth + window.inner
 var maxAccuracy = 0;
 var minAccuracy = 100;
 var totalAccuracy = 0;
-var maxPhases = 1;
-var numberOfPhases = maxPhases;
 const results = [];
 const distances = [];
 var centerTouchX = 0.5 * window.innerWidth;
 var centerTouchY = 0.5 * window.innerHeight;
 var resultText = "";
-var currentPhase = ["Full Screen 1: ", "Full Screen 2: ", "Full Screen 3: ", "Big Dot 1: ", "Big Dot 2: ", "Big Dot 3: ",
-    "Big Dot 4: ", "Small Dot 1: ", "Small Dot 2: ", "Small Dot 3: ", "Small Dot 4: ", "Tiny Dot 1: ",
-    "Tiny Dot 2: ", "Tiny Dot 3: ", "Tiny Dot 4: "];
+var currentPhase = [];
 var section = 0;
 var block = 0;
+var pause = false;
+var dropSeconds = 2;
 
 // Function to calculate distance between two points
 function calculateDistance(x1, y1, x2, y2) {
@@ -29,14 +27,18 @@ function calculateDistance(x1, y1, x2, y2) {
     return Math.sqrt(dx * dx + dy * dy);
 }
 
+// Function to calculate accuracy of tap
 function calculateAccuracy(tapDistance) {
     var acc = (maxDistance - tapDistance) / maxDistance;
     return acc * 100;
 }
 
+// Function to define touch start and touch end functions
 function defineElements() {
     document.getElementById("touchSpace").ontouchstart = function (e) {
-        touchStarted = touchStarted + 1;
+        if (secondsLeft < maxTime - dropSeconds) {
+            touchStarted = touchStarted + 1;
+        }
     };
 
     document.getElementById("touchSpace").ontouchend = function (e) {
@@ -61,15 +63,17 @@ function defineElements() {
     };
 }
 
+// Function to store results after each block
 function storeResults() {
     var averageAccuracy = totalAccuracy / touchCount;
     if (minAccuracy > maxAccuracy) {
         minAccuracy = 0;
     }
-    results[maxPhases - numberOfPhases - 1] = [touchCount, Math.round(maxAccuracy * 10000) / 10000, Math.round(minAccuracy * 10000) / 10000, Math.round(averageAccuracy * 10000) / 10000,]
-    resultText = resultText + "<br />" + currentPhase[maxPhases - numberOfPhases - 1] + touchCount.toString();
+    results[block - 1] = [touchCount, Math.round(maxAccuracy * 10000) / 10000, Math.round(minAccuracy * 10000) / 10000, Math.round(averageAccuracy * 10000) / 10000,]
+    resultText = resultText + "<br />" + currentPhase[block - 1] + touchCount.toString();
 }
 
+// Function to reset all variables at start of each new block
 function resartPhase() {
     secondsLeft = maxTime;
     touchStarted = 0;
@@ -81,133 +85,211 @@ function resartPhase() {
     document.getElementById("count").innerHTML = 0;
     document.getElementById("resultPanel").style.visibility = "hidden";
     secondsLeft = maxTime;
+    pause = false;
 }
 
+// Function to continue to next block when button is clicked on
+function onClickContinue(infoBlockName) {
+    document.getElementById(infoBlockName).style.visibility = "hidden";
+    document.getElementById("touchBackground").style.visibility = "visible";
+    resartPhase();
+}
+
+// Function to define new block based on section/block number - shows info page for each block
+function newBlock() {
+    pause = true;
+    document.getElementById("touchBackground").style.visibility = "hidden";
+    document.getElementById("touchSpace").ontouchstart = null;
+    document.getElementById("touchSpace").ontouchend = null;
+
+    // section 1
+    if (section === 1) {
+        if (block === 2) {
+            document.getElementById("infoPanel-2").style.visibility = "visible";
+        }
+        if (block === 3) {
+            document.getElementById("infoPanel-3").style.visibility = "visible";
+        }
+    }
+
+    // section 2
+    if (section === 2) {
+        if (block === 2) {
+            document.getElementById("infoPanel-2").style.visibility = "visible";
+        }
+    }
+
+    // section 3
+    if (section === 3) {
+        if (block === 2) {
+            document.getElementById("infoPanel-2").style.visibility = "visible";
+        }
+    }
+
+    // section 4
+    if (section === 4) {
+        if (block === 2) {
+            document.getElementById("infoPanel-2").style.visibility = "visible";
+        }
+    }
+}
+
+// Function to display results at end of each section
 function displayResults() {
+    pause = true;
+    document.getElementById("touchBackground").style.visibility = "hidden";
     document.getElementById("touchSpace").ontouchstart = null;
     document.getElementById("touchSpace").ontouchend = null;
     document.getElementById("touchSpace").onmousedown = null;
     document.getElementById("touchSpace").onmouseup = null;
     document.getElementById("resultPanel").style.visibility = "visible";
     document.getElementById("resultsText").innerHTML = resultText;
-    document.getElementById("restart").onclick = function (e) {
-        numberOfPhases = 1;
-        document.getElementById("touchSpace").style.width = "100%";
-        document.getElementById("touchSpace").style.height = "100%";
-        document.getElementById("touchSpace").style.borderRadius = "0%";
-        resultText = "";
-        resartPhase();
-    }
 }
 
+// Function to change position of dots randomly
 function changePosition() {
-    maxTime = 2;
     newWidth = Math.random() * (85 - 15) + 15;
     newHeight = Math.random() * (85 - 15) + 15;
-    centerTouchX = newWidth * window.innerWidth;
-    centerTouchY = newHeight * window.innerHeight;
+    centerTouchX = newWidth * window.innerWidth + (document.getElementById("touchBackground").style.width / 2);
+    centerTouchY = newHeight * window.innerHeight + (document.getElementById("touchBackground").style.height / 2);
     document.getElementById("touchSpace").style.top = newHeight.toString() + "%";
     document.getElementById("touchSpace").style.left = newWidth.toString() + "%";
 }
 
+// Function to reset position to center of screen
 function resetPosition() {
-    maxTime = 6;
     document.getElementById("touchSpace").style.top = "50%";
     document.getElementById("touchSpace").style.left = "50%";
 }
 
-/*
+// Timer function that runs every second
 function timerFunction() {
+
+    // Define elements at start of block
     if (secondsLeft === maxTime) {
         defineElements();
     }
+
+    // Store results, reset position and proceed to next block when timer ends
     if (secondsLeft === 0) {
-        document.getElementById("timer").innerHTML = secondsLeft + "s";
-        clearInterval(timer);
-        numberOfPhases = numberOfPhases - 1;
-        storeResults();
-        resetPosition();
-        if (numberOfPhases === 0) {
-            displayResults();
-        }
-        // big circle - multi finger
-        if (numberOfPhases === maxPhases - 3) {
-            document.getElementById("touchSpace").style.width = "1.25in";
-            document.getElementById("touchSpace").style.height = "1.25in";
-            document.getElementById("touchSpace").style.borderRadius = "50%";
-            resartPhase();
-        }
-        // big moving circle - multi finger
-        if (numberOfPhases <= maxPhases - 4 && numberOfPhases >= maxPhases - 6) {
-            changePosition();
-            resartPhase();
-        }
+        if (pause === false) {
+            var audio = document.getElementById("audio");
+            audio.play();
+            document.getElementById("timer").innerHTML = secondsLeft + "s";
+            clearInterval(timer);
+            storeResults();
+            resetPosition();
 
-        // small circle - finger size
-        if (numberOfPhases === maxPhases - 7) {
-            document.getElementById("touchSpace").style.width = "0.7in";
-            document.getElementById("touchSpace").style.height = "0.7in";
-            document.getElementById("touchSpace").style.borderRadius = "50%";
-            resartPhase();
-        }
-        // small moving circle - multi finger
-        if (numberOfPhases <= maxPhases - 8 && numberOfPhases >= maxPhases - 10) {
-            changePosition();
-            resartPhase();
-        }
+            // section 1
+            if (section === 1) {
+                block = block + 1;
+                if (block <= 3) {
+                    newBlock();
+                }
+                if (block === 4) {
+                    displayResults();
+                }
+            }
 
-        // tiny circle - half finger size
-        if (numberOfPhases === maxPhases - 11) {
-            document.getElementById("touchSpace").style.width = "0.35in";
-            document.getElementById("touchSpace").style.height = "0.35in";
-            document.getElementById("touchSpace").style.borderRadius = "50%";
-            resartPhase();
-        }
-        // tiny moving circle - multi finger
-        if (numberOfPhases <= maxPhases - 12 && numberOfPhases >= maxPhases - 14) {
-            changePosition();
-            resartPhase();
-        }
+            // section 2
+            if (section === 2) {
+                block = block + 1;
+                if (block === 2) {
+                    newBlock();
+                }
+                if (block === 3) {
+                    displayResults();
+                }
+            }
 
-        if (numberOfPhases >= maxPhases - 2) {
-            resartPhase();
-        }
+            // section 3
+            if (section === 3) {
+                block = block + 1;
+                if (block === 2) {
+                    newBlock();
+                }
+                if (block === 3) {
+                    displayResults();
+                }
+            }
 
-    }
-    else {
-        document.getElementById("timer").innerHTML = secondsLeft + "s";
-        secondsLeft--;
-    }
-}
-*/
-
-function timerFunction() {
-    if (secondsLeft === maxTime) {
-        defineElements();
-    }
-    if (secondsLeft === 0) {
-        document.getElementById("timer").innerHTML = secondsLeft + "s";
-        clearInterval(timer);
-        numberOfPhases = numberOfPhases - 1;
-        storeResults();
-        resetPosition();
-        if (numberOfPhases === 0) {
-            displayResults();
+            // section 4
+            if (section === 4) {
+                block = block + 1;
+                if (block === 2) {
+                    newBlock();
+                }
+                if (block === 3) {
+                    displayResults();
+                }
+            }
         }
     }
     else {
+        // print timer 
         document.getElementById("timer").innerHTML = secondsLeft + "s";
         secondsLeft--;
+
+        // change positions of dot randomly whenever required
+        if (section != 1) {
+            if (block === 2) {
+                if (secondsLeft % 2 === 0) {
+                    changePosition();
+                }
+            }
+        }
     }
 }
 
+// Function to start a session - defines all section related variable and starts timer for block 1.
 function onClickStart(sectionBlock) {
-    if (sectionBlock === "s1b1") {
-        document.getElementById("infoPanel").style.visibility = "hidden";
+    // section 1
+    if (sectionBlock === "s1") {
+        section = 1;
+        block = 1;
+        currentPhase = ["Full Screen Normal: ", "Full Screen Away From Screen: ", "Full Screen Eyes Closed: "];
+        document.getElementById("infoPanel-1").style.visibility = "hidden";
+        document.getElementById("touchBackground").style.visibility = "visible";
+        const timer = setInterval(timerFunction, 1000);
+    }
+
+    // section 2
+    if (sectionBlock === "s2") {
+        section = 2;
+        block = 1;
+        currentPhase = ["Big Dot Normal: ", "Big Dot Dynamic: "];
+        document.getElementById("touchSpace").style.width = "1.25in";
+        document.getElementById("touchSpace").style.height = "1.25in";
+        document.getElementById("touchSpace").style.borderRadius = "50%";
+        document.getElementById("infoPanel-1").style.visibility = "hidden";
+        document.getElementById("touchBackground").style.visibility = "visible";
+        const timer = setInterval(timerFunction, 1000);
+    }
+
+    // section 3
+    if (sectionBlock === "s3") {
+        section = 3;
+        block = 1;
+        currentPhase = ["Small Dot Normal: ", "Small Dot Dynamic: "];
+        document.getElementById("touchSpace").style.width = "0.7in";
+        document.getElementById("touchSpace").style.height = "0.7in";
+        document.getElementById("touchSpace").style.borderRadius = "50%";
+        document.getElementById("infoPanel-1").style.visibility = "hidden";
+        document.getElementById("touchBackground").style.visibility = "visible";
+        const timer = setInterval(timerFunction, 1000);
+    }
+
+    // section 4
+    if (sectionBlock === "s4") {
+        section = 4;
+        block = 1;
+        currentPhase = ["Tiny Dot Normal: ", "Tiny Dot Dynamic: "];
+        document.getElementById("touchSpace").style.width = "0.35in";
+        document.getElementById("touchSpace").style.height = "0.35in";
+        document.getElementById("touchSpace").style.borderRadius = "50%";
+        document.getElementById("infoPanel-1").style.visibility = "hidden";
         document.getElementById("touchBackground").style.visibility = "visible";
         const timer = setInterval(timerFunction, 1000);
     }
 
 }
-
-
